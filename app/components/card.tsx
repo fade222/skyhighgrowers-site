666e012d8cdbaf7c2872cd533e4525f1a8097571
fade222 <43367106+fade222@users.noreply.github.com>
2024-10-06 03:16:51 +1300

    increase font size

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -20,7 +20,7 @@ export default function card({
   cropDetails,
 }: CropCardProps) {
   return (
-    <div className="grid grid-cols-6 font-sans m-10 lg:mx-96 lg:h-64 rounded-xl shadow-lg shadow-green-800/20 border isolate bg-white">
+    <div className="grid grid-cols-6 font-sans m-4 lg:m-16 lg:mx-96 lg:min-h-64 rounded-xl shadow-lg shadow-green-800/20 border isolate bg-white ">
       <div className="col-span-2 relative">
         <Image
           className=" rounded-l-xl object-cover"
@@ -30,21 +30,19 @@ export default function card({
         />
       </div>
 
-      <div className="grid col-span-4 pl-2 lg:p-4 grid-rows-3">
+      <div className="grid col-span-4 pl-2 lg:p-4  grid-rows-3">
         <div className="lg:flex lg:items-baseline">
           <h1 className="text-2xl lg:text-4xl font-semibold text-slate-900">
             {name}
           </h1>
 
-          <h2 className="lg:ml-2 text-sm lg:text-lg italic -mt-1 text-slate-700 ">
+          <h2 className="lg:ml-2 text-sm lg:text-xl italic -mt-1 text-slate-700 ">
             {scientificName}
           </h2>
         </div>
-        <p className="mt-4 text-slate-900 text-sm lg:text-base">
-          {cropDetails}
-        </p>
+        <p className="mt-4 text-slate-900 text-sm lg:text-xl">{cropDetails}</p>
 
-        <div className="mt-4 mb-2 text-sm lg:text-base font-medium  text-slate-900">
+        <div className="mt-4 mb-2 text-sm lg:text-lg font-medium  text-slate-900">
           Harvest:
           <span className="pl-1 font-normal text-slate-900">
             {dateFormatter(harvestDate, "MMM dd, yyyy")}
